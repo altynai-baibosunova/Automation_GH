9f1b8d2d8f71e722915086ca7116cdfbc5797fd8
Altynai <harmoni5413@gmail.com>
2025-08-12 15:27:47 -0400

    add color

--- CandyCrush/script.js
+++ CandyCrush/script.js
@@ -1,8 +1,20 @@
-
 const board = document.getElementById("game-board");
 
-for (let i = 0; i < 64; i++) { // 8x8 grid
+const candyColors = [
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "orange",
+  "purple"
+];
+
+for (let i = 0; i < 64; i++) { 
   const square = document.createElement("div");
   square.classList.add("square");
+
+  let randomColor = candyColors[Math.floor(Math.random() * candyColors.length)];
+  square.style.backgroundColor = randomColor;
+
   board.appendChild(square);
 }

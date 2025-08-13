05a43161f0b3c7e35c5326488d3d84f806e62bad
Altynai <harmoni5413@gmail.com>
2025-08-13 09:15:32 -0400

    images added

--- CandyCrush/script.js
+++ CandyCrush/script.js
@@ -1,20 +1,20 @@
 const board = document.getElementById("game-board");
 
-const candyColors = [
-  "red",
-  "yellow",
-  "green",
-  "blue",
-  "orange",
-  "purple"
-];
+const candies = ["red.png", "green.png", "orange.png"];
 
-for (let i = 0; i < 64; i++) { 
+for (let i = 0; i < 64; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
 
-  let randomColor = candyColors[Math.floor(Math.random() * candyColors.length)];
-  square.style.backgroundColor = randomColor;
+  // Pick a random candy
+  const randomCandy = candies[Math.floor(Math.random() * candies.length)];
 
+  // Create image element
+  const candyImg = document.createElement("img");
+  candyImg.src = `images/${randomCandy}`;
+  candyImg.alt = "Candy";
+  candyImg.classList.add("candy");
+
+  square.appendChild(candyImg);
   board.appendChild(square);
 }
